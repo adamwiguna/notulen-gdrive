cd73a097199051c2cc9fb1421f3d9d64d6a45758
adamwiguna <adamwiguna56@gmail.com>
2022-08-16 12:07:36 +0800

    Update PhotoController.php

--- app/Http/Controllers/User/PhotoController.php
+++ app/Http/Controllers/User/PhotoController.php
@@ -49,7 +49,7 @@ class PhotoController extends Controller
        
 
         try {
-            dd($request->photo);
+            // dd($request->photo);
             $content = $request->file('photo')->getContent();
             Storage::disk('google')->put($image_full_name, $content);
             session()->flash('message-gdrive' , 'Foto berhasil dibackup ke Google-Drive');
